132f3614d7afb2dc49a9d3667adbdd0b97e7f6c6
Petra Řezáčová <ing.rezacova@gmail.com>
2022-04-26 20:52:08 +0200

    Metody pro čtverec, obdélník, rovnostranný trojúhelník a kolečko.

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -2,12 +2,80 @@ package cz.czechitas.turtle;
 
 import cz.czechitas.turtle.engine.*;
 
+import java.awt.*;
+
 public class HlavniProgram {
 
     public void main(String[] args) {
         Turtle zofka;
 
         zofka = new Turtle();
+        zofka.setPenColor(Color.black);
+
+
+        nakresliCtverecek(zofka, 100.0);
+
+        zofka.setLocation(200,200);
+
+        nakresliObdelnik(zofka, 200.0, 100.0 );
+
+        zofka.turnRight(90);
+        zofka.penUp();
+        zofka.move(600);
+        zofka.penDown();
+        zofka.turnLeft(90);
+
+
+        nakresliRovnostrannyTrojuhelnik(zofka, 100.0);
+
+        zofka.penUp();
+        zofka.move(200);
+        zofka.penDown();
+
+        nakresliKolecko(zofka, 5.0);
+
+
+
     }
 
+    private void nakresliKolecko(Turtle turtle, double velikostStrany) {
+        for (int i = 0; i < 17 ; i++) {
+            turtle.move(velikostStrany);
+            turtle.turnRight(20);
+
+        }
+    }
+
+    private void nakresliRovnostrannyTrojuhelnik(Turtle turtle, double velikostStrany) {
+        for (int i = 0; i < 3; i++) {
+            turtle.move(velikostStrany);
+            turtle.turnLeft(120.0);
+        }
+    }
+
+    private void nakresliObdelnik(Turtle turtle, double delkaStrany, double sirkaStrany) {
+        turtle.turnRight(90);
+        turtle.move(delkaStrany);
+        turtle.turnRight(90);
+        turtle.move(sirkaStrany);
+        turtle.turnRight(90);
+        turtle.move(delkaStrany);
+        turtle.turnRight(90);
+        turtle.move(sirkaStrany);
+        turtle.turnRight(180);
+        turtle.penUp();
+        turtle.move(100);
+        turtle.turnRight(180);
+    }
+
+    private void nakresliCtverecek(Turtle turtle, double delkaStrany) {
+        for (int i = 0; i < 4; i++) {
+            turtle.move(delkaStrany);
+            turtle.turnRight(90);
+        }
+    }
+
+
 }
+
+
